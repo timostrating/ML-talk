9ae348f88c21c89b6d66c7d0774ecf8d3d7453a7
timostrating <timostrating@home.nl>
2018-05-13 23:38:07 +0200

    unity init

---
+++ NeuralNetwork-Visualisation/Assets/Code/LineTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineTest : MonoBehaviour {
+
+	LineRenderer lr;
+
+
+	void Start() {
+		lr = GetComponent<LineRenderer>();
+	}
+
+	void Update () {
+		var t = Time.time;
+		for(int i=0; i<10; i++) {
+			lr.SetPosition(i, new Vector3(i * 0.5f, Mathf.Sin(i + t), 0.0f));
+		}
+	}
+}
